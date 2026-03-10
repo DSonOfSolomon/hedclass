b5b38e9fd6cede91e50c29c54666a79b54df5ec3
DC(DSonOfSolomon) <danielchioma1@gmail.com>
2026-03-10 00:20:44 +0000

    Add EJS templating and homepage view

--- src/web/app.js
+++ src/web/app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("HEdClass Web App Running");
+    res.render("index");
 });
 
 app.listen(PORT, () => {
