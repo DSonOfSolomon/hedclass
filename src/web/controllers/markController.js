54b17589157fccbceb439e1cc528686fbe77862d
DC(DSonOfSolomon) <danielchioma1@gmail.com>
2026-03-14 23:50:30 +0000

    Add edit and delete functionality for marks

--- src/web/controllers/markController.js
+++ src/web/controllers/markController.js
@@ -4,70 +4,132 @@ const db = require("../models/db");
 List all marks
 */
 exports.listMarks = (req, res) => {
-
-    const sql = `
+  const sql = `
     SELECT marks.*, students.name AS student_name, modules.name AS module_name
     FROM marks
     LEFT JOIN students ON marks.student_id = students.id
     LEFT JOIN modules ON marks.module_id = modules.id
     `;
 
-    db.query(sql, (err, results) => {
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.send("Database error");
+    }
 
-        if (err) {
-            console.error(err);
-            return res.send("Database error");
-        }
-
-        res.render("marks", { marks: results });
-
-    });
-
+    res.render("marks", { marks: results });
+  });
 };
-
 
 /*
 Show mark entry form
 */
 exports.showCreateMark = (req, res) => {
+  const studentQuery = "SELECT * FROM students";
+  const moduleQuery = "SELECT * FROM modules";
 
-    const studentQuery = "SELECT * FROM students";
-    const moduleQuery = "SELECT * FROM modules";
+  db.query(studentQuery, (err, students) => {
+    if (err) return res.send("Error loading students");
 
-    db.query(studentQuery, (err, students) => {
+    db.query(moduleQuery, (err, modules) => {
+      if (err) return res.send("Error loading modules");
 
-        if (err) return res.send("Error loading students");
-
-        db.query(moduleQuery, (err, modules) => {
-
-            if (err) return res.send("Error loading modules");
-
-            res.render("create_mark", { students, modules });
-
-        });
-
+      res.render("create_mark", { students, modules });
     });
-
+  });
 };
-
 
 /*
 Save mark
 */
 exports.createMark = (req, res) => {
+  const { student_id, module_id, mark, is_resit } = req.body;
 
-    const { student_id, module_id, mark, is_resit } = req.body;
-
-    const sql = `
+  const sql = `
     INSERT INTO marks (student_id, module_id, mark, is_resit)
     VALUES (?, ?, ?, ?)
     `;
 
-    db.query(sql, [student_id, module_id, mark, is_resit ? 1 : 0], (err) => {
+  db.query(sql, [student_id, module_id, mark, is_resit ? 1 : 0], (err) => {
+    if (err) {
+      console.error(err);
+      return res.send("Error saving mark");
+    }
+
+    res.redirect("/marks");
+  });
+};
+
+/*
+Show edit mark form
+*/
+exports.showEditMark = (req, res) => {
+  const id = req.params.id;
+
+  const markQuery = "SELECT * FROM marks WHERE id = ?";
+  const studentQuery = "SELECT * FROM students";
+  const moduleQuery = "SELECT * FROM modules";
+
+  db.query(markQuery, [id], (err, markResult) => {
+    if (err) {
+      console.error(err);
+      return res.send("Database error");
+    }
+
+    db.query(studentQuery, (err, students) => {
+      if (err) return res.send("Error loading students");
+
+      db.query(moduleQuery, (err, modules) => {
+        if (err) return res.send("Error loading modules");
+
+        res.render("edit_mark", {
+          mark: markResult[0],
+          students,
+          modules,
+        });
+      });
+    });
+  });
+};
+
+/*
+Update mark
+*/
+exports.updateMark = (req, res) => {
+  const id = req.params.id;
+
+  const { student_id, module_id, mark, is_resit } = req.body;
+
+  const sql = `
+    UPDATE marks
+    SET student_id = ?, module_id = ?, mark = ?, is_resit = ?
+    WHERE id = ?
+    `;
+
+  db.query(sql, [student_id, module_id, mark, is_resit ? 1 : 0, id], (err) => {
+    if (err) {
+      console.error(err);
+      return res.send("Error updating mark");
+    }
+
+    res.redirect("/marks");
+  });
+};
+
+/*
+Delete mark
+*/
+exports.deleteMark = (req, res) => {
+
+    const id = req.params.id;
+
+    const sql = "DELETE FROM marks WHERE id = ?";
+
+    db.query(sql, [id], (err) => {
 
         if (err) {
             console.error(err);
-            return res.send("Error saving mark");
+            return res.send("Error deleting mark");
         }
 
         res.redirect("/marks");
--- src/web/routes/markRoutes.js
+++ src/web/routes/markRoutes.js
@@ -7,28 +7,51 @@ const authMiddleware = require("../middleware/authMiddleware");
 /*
 View marks
 */
-router.get(
-"/marks",
-authMiddleware.isAuthenticated,
-markController.listMarks
-);
+router.get("/marks", authMiddleware.isAuthenticated, markController.listMarks);
 
 /*
 Show mark entry form
 */
 router.get(
-"/marks/create",
-authMiddleware.isAuthenticated,
-markController.showCreateMark
+  "/marks/create",
+  authMiddleware.isAuthenticated,
+  markController.showCreateMark
 );
 
 /*
 Save mark
 */
 router.post(
-"/marks/create",
-authMiddleware.isAuthenticated,
-markController.createMark
+  "/marks/create",
+  authMiddleware.isAuthenticated,
+  markController.createMark
+);
+
+/*
+Show edit mark form
+*/
+router.get(
+  "/marks/edit/:id",
+  authMiddleware.isAuthenticated,
+  markController.showEditMark
+);
+
+/*
+    Update mark
+    */
+router.post(
+  "/marks/update/:id",
+  authMiddleware.isAuthenticated,
+  markController.updateMark
+);
+
+/*
+Delete mark
+*/
+router.get(
+  "/marks/delete/:id",
+  authMiddleware.isAuthenticated,
+  markController.deleteMark
 );
 
 module.exports = router;
